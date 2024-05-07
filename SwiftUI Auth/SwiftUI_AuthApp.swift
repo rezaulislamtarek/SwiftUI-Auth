8565aa52e19b5c7b5eb4c5d8e13c2cc6d26455a2
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-05-07 12:19:08 +0600

    Initial Commit

---
+++ SwiftUI Auth/SwiftUI_AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AuthApp.swift
+//  SwiftUI Auth
+//
+//  Created by Rezaul Islam on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
